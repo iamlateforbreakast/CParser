adce1f4dbd0b95e035811d83bcae145d8bdc71d1
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2017-04-19 09:28:59 +0100

    Added content

--- src/main.c
+++ src/main.c
@@ -1,1 +1,24 @@
-/* main.c */
+/* Main */
+
+#include "Common.h"
+#include "CParser.h"
+
+int main(int argc, char** argv)
+{
+    CParser* cparser;    
+    int result = 0;
+
+    if (argc == 2)
+    {
+        cparser=CParser_new();
+        CParser_parse(cparser, argv[1]);
+        CParser_delete(cparser);
+	printf("NbBytesAllocated: %d\n", Memory_nbBytesAllocated);
+    }
+    else
+    {
+	printf("Usage: cparser filename.c\n");
+    }
+
+    return result;
+}
