334339c50963cab63e186d95baeae01b7dbbf0fd
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2017-04-19 09:31:19 +0100

    Initial revision

---
+++ src/CParser.h
@@ -0,0 +1,20 @@
+/* CParser.h */
+
+#ifndef _CPARSER_H_
+#define _CPARSER_H_
+
+//#include "ElementTable.h"
+//
+typedef struct CParser
+{
+    // ElementTable functions
+    // ElementTable global vars
+    // ElementTable local vars
+    // ElementTable code blocks
+} CParser;
+
+CParser* CParser_new();
+void CParser_delete(CParser* this);
+void CParser_parse(CParser* this, const char* fileName);
+
+#endif
