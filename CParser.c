c4990ce4ddc3db1e31ae95a4277cfdb95fc42cd6
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2017-04-25 00:10:23 +0100

    Corrected

---
+++ CParser.c
@@ -0,0 +1,59 @@
+/* CParser.c */
+
+#include "Common.h"
+#include "CParser.h"
+#include "FileList.h"
+
+CParser* CParser_new()
+{
+    CParser* this;
+
+    this=(CParser*)Memory_alloc(sizeof(CParser));
+
+    this->tokenList = TokenList_new();
+    this->fileList = FileList_new();
+
+    return this;
+}
+
+void CParser_delete(CParser* this)
+{
+    TokenList_delete(this->tokenList);
+    FileList_delete(this->fileList);
+
+    Memory_free(this, sizeof(CParser));
+}
+
+void CParser_parse(CParser* this, const char* dirName)
+{
+    String* newDirName, *filter;
+
+    newDirName = String_new(dirName);
+    filter = String_new(".c");
+
+    FileList_list(this->fileList, newDirName, filter);
+
+    // List all C files in newDirName and sub directories
+    // for each C file add to the TokenList
+    //Initialise from initial fileName
+    TokenList_initialise(this->tokenList, newDirName);
+    
+    /*while (TokensList_get())
+	populate functions from list of tokens
+	populate global vars
+	populate local vars
+	populate code blocks */
+    String_delete(newDirName);
+    String_delete(filter);
+}
+
+/*void CParser_populateFunctions()
+{
+    if Grammar_matchFunctions(element)
+    {
+    }
+}
+
+void CParser_populateGlobalVars()
+{
+}*/
