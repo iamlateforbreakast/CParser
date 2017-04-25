fca4f14f2465c494432656bece80b920808e2309
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2017-04-25 18:14:04 +0100

    Corrected problems in listing files

---
+++ FileList.h
@@ -0,0 +1,29 @@
+/* FileList.h */
+
+#ifndef _FILELIST_H_
+#define _FILELIST_H_
+
+#include "String.h"
+
+typedef struct FileInfo FileInfo;
+
+struct FileInfo
+{
+    String* name;
+    String* path;
+    //FileType type;
+    //FileDate date;
+    //FileSize size
+    FileInfo* next;
+};
+
+typedef struct FileList
+{
+    FileInfo* head;
+} FileList;
+
+FileList* FileList_new();
+void FileList_delete(FileList* this);
+void FileList_addFile(FileList* this, String* fileName, String* path);
+void FileList_list(FileList* this, String* dirName, String* filter);
+#endif
---
+++ Memory.c
@@ -0,0 +1,27 @@
+/* Memory.c */
+
+#include "Common.h"
+
+unsigned int Memory_nbBytesAllocated = 0;
+
+void* Memory_alloc(unsigned int nbBytes)
+{
+    void* p = NULL;
+
+    //printf("Malloc: %d\n", nbBytes);
+    p = malloc(nbBytes);
+    Memory_nbBytesAllocated += nbBytes;
+
+    return p;
+}
+
+void Memory_free(void* p, unsigned int nbBytes)
+{
+    if (p!=NULL)
+    {
+        //printf("Free: %d\n", nbBytes);
+        free(p);
+
+        Memory_nbBytesAllocated = Memory_nbBytesAllocated - nbBytes;
+    }
+}
