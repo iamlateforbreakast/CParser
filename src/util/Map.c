1e7a7c9014309b7bb704c0119331feb1d0e0ef91
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2017-06-07 13:45:26 +0100

    Initial revision

---
+++ src/util/Map.c
@@ -0,0 +1,16 @@
+/* Map.c */
+
+#include "Map.h"
+
+#include "Common.h"
+
+Map* Map_new()
+{
+  Map* this = NULL;
+
+  return this;
+}
+
+void Map_delete(Map* this)
+{
+}
---
+++ src/util/Map.h
@@ -0,0 +1,13 @@
+/* Map.h */
+
+#ifndef _MAP_H_
+#define _MAP_H_
+
+typedef struct Map
+{
+} Map;
+
+Map* Map_new();
+void Map_delete(Map* this);
+
+#endif
