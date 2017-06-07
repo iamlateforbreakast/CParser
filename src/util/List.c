45dfa66b5d129329e67a57128625c30958a09205
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2017-06-07 13:45:54 +0100

    Initial revision

---
+++ src/util/List.c
@@ -0,0 +1,16 @@
+/* List.c */
+
+#include "List.h"
+
+#include "Common.h"
+
+List* List_new()
+{
+  List* this = NULL;
+
+  return this;
+}
+
+void List_delete(List* this)
+{
+}
---
+++ src/util/List.h
@@ -0,0 +1,13 @@
+/* List.h */
+
+#ifndef _LIST_H_
+#define _LIST_H_
+
+typedef struct List
+{
+} List;
+
+List* List_new();
+void List_delete(List* this);
+
+#endif
