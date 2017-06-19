054f25582b0a3d5c0a84b3deb23f30c60d1eee0c
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2017-06-19 12:37:27 +0100

    Implement List object

--- src/util/List.c
+++ src/util/List.c
@@ -3,14 +3,49 @@
 #include "List.h"
 
 #include "Common.h"
-
+ 
 List* List_new()
 {
   List* this = NULL;
 
+  this = (List*)Memory_alloc(sizeof(List));
   return this;
 }
 
 void List_delete(List* this)
 {
+  Memory_free(this, sizeof(List));   
+}
+
+void List_insert(List* this, void* item)
+{
+  ListNode* newNode = NULL;
+  
+  newNode = Memory_alloc(sizeof(ListNode));
+  newNode->item = item;
+  newNode->next = this->head;
+  newNode->prev = NULL;
+  this->head = newNode;
+}
+
+unsigned int List_find(List* this, void* item)
+{
+  unsigned int result = 0;
+  
+  return result;
+}
+
+void List_remove(List* this)
+{
+}
+
+void List_iterator(List* this, void *(f)(void* t))
+{
+  ListNode* iterator = this->head;
+  
+  while (iterator!=NULL)
+  {
+    f(iterator->item);
+    iterator = iterator->next;
+  }
 }
--- src/util/List.h
+++ src/util/List.h
@@ -3,11 +3,22 @@
 #ifndef _LIST_H_
 #define _LIST_H_
 
+typedef struct ListNode ListNode;
+
+struct ListNode
+{
+  void* item;
+  ListNode* next;
+  ListNode* prev;
+};
+
 typedef struct List
 {
+  ListNode* head;
+  ListNode* current;
 } List;
 
 List* List_new();
 void List_delete(List* this);
-
+void List_insert(List* this, void* item);
 #endif
