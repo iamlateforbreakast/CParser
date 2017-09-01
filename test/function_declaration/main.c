4645f35dbff292c6b7672c635c97dcbcafb2c389
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2017-09-01 17:50:10 +0100

    Add test

--- test/function_declaration/main.c
+++ test/function_declaration/main.c
@@ -1,1 +1,15 @@
+int function_name1();
 
+void function_name2(int param1);
+
+float function_name3()
+{
+    float ret_val3;
+
+    return ret_val3;
+}
+
+void function_name4(int param4)
+{
+    function_name2(param4);
+}
