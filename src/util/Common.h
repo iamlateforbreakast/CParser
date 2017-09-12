7defefea114c2f77cadbd46b69de602b72604e05
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2017-09-12 10:35:57 +0100

    Add TRACE macro

--- src/util/Common.h
+++ src/util/Common.h
@@ -4,6 +4,7 @@
 #define _COMMON_
 
 #include <stdio.h>
+#include <stdarg.h>
 #include <stdlib.h>
 
 #include "String2.h"
@@ -11,5 +12,15 @@
 
 #define PRIVATE static
 #define PUBLIC
+
+#define TRACE(x) do { if (DEBUG) dbg_printf x; } while (0)
+
+void dbg_printf(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+}
 
 #endif
