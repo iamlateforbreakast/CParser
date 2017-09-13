994e28753336f98868c2151357d693cd15becc88
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2017-09-13 17:18:38 +0100

    Initial revision

---
+++ src/util/Debug.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+void dbg_printf(const char *fmt, ...)
+{
+    va_list args;
+    va_start(args, fmt);
+    vfprintf(stderr, fmt, args);
+    va_end(args);
+}
