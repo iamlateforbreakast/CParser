6beb48a3c215913cdf58ed1208e9188da9da5cd3
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2017-09-13 17:16:21 +0100

    Add TRACE

--- src/util/Common.h
+++ src/util/Common.h
@@ -15,12 +15,4 @@
 
 #define TRACE(x) do { if (DEBUG) dbg_printf x; } while (0)
 
-void dbg_printf(const char *fmt, ...)
-{
-    va_list args;
-    va_start(args, fmt);
-    vfprintf(stderr, fmt, args);
-    va_end(args);
-}
-
 #endif
