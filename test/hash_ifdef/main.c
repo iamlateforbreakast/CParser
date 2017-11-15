7e6f121b2ae87659223edace897c61373962778e
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2017-11-15 22:55:34 +0000

    Initial revision

--- test/hash_ifdef/main.c
+++ test/hash_ifdef/main.c
@@ -1,1 +1,31 @@
+#define TEST1
 
+#ifdef TEST1
+
+void success1()
+{
+}
+
+#else
+
+void fail1()
+{
+}
+
+#endif
+
+#define TEST2
+
+#ifndef TEST2
+
+void fail2()
+{
+}
+
+#else
+
+void succes2()
+{
+}
+
+#endif
