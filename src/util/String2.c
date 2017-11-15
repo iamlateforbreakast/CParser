c5fcd9c6a0c933310d491b1167a3fa15ac27bbb9
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2017-11-15 16:59:00 +0000

    Add search and replace API

--- src/util/String2.c
+++ src/util/String2.c
@@ -222,6 +222,82 @@ int String_toInt(String* this)
 String* String_searchAndReplace(String* this, String* search, String* replace)
 {
   String* result = NULL;
+  String* tmpStr = NULL;
+  unsigned int i,j;
   
+  i = 0;
+  j = 0;
+  
+  if ((this==NULL) || (search==NULL) || (replace==NULL))
+  {
+    result = this;
+  }
+  else
+  {
+    while ( (j+search->length)<this->length)
+    {
+      if (String_match(this,j,search))
+      {
+        if (j==0)
+        {
+          result = String_dup(replace);
+        }
+        else
+        {
+          tmpStr = String_subString(this, i, j-i);
+          String_append(tmpStr, replace);
+          String_append(result, tmpStr);
+        } 
+
+        j = j + search->length;
+        i = j;
+      }
+      else
+      {
+        j++;
+      }
+    }
+    tmpStr = String_subString(this, i, j-i);
+    String_append(result, tmpStr);
+  }
+
   return result;
 }
+
+#if 0
+    /* match: search for regexp anywhere in text */
+    int match(char *regexp, char *text)
+    {
+        if (regexp[0] == '^')
+            return matchhere(regexp+1, text);
+        do {    /* must look even if string is empty */
+            if (matchhere(regexp, text))
+                return 1;
+        } while (*text++ != '\0');
+        return 0;
+    }
+
+    /* matchhere: search for regexp at beginning of text */
+    int matchhere(char *regexp, char *text)
+    {
+        if (regexp[0] == '\0')
+            return 1;
+        if (regexp[1] == '*')
+            return matchstar(regexp[0], regexp+2, text);
+        if (regexp[0] == '$' && regexp[1] == '\0')
+            return *text == '\0';
+        if (*text!='\0' && (regexp[0]=='.' || regexp[0]==*text))
+            return matchhere(regexp+1, text+1);
+        return 0;
+    }
+
+    /* matchstar: search for c*regexp at beginning of text */
+    int matchstar(int c, char *regexp, char *text)
+    {
+        do {    /* a * matches zero or more instances */
+            if (matchhere(regexp, text))
+                return 1;
+        } while (*text != '\0' && (*text++ == c || c == '.'));
+        return 0;
+    }
+    #endif
