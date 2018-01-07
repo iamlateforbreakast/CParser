33d321d0b192aa5e16d450df31c1073fbcea07ea
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-01-07 23:23:25 +0000

    Make object struct public

--- src/util/Object.c
+++ src/util/Object.c
@@ -3,12 +3,6 @@
 #include "Common.h"
 #include "Object.h"
 
-struct Object
-{
-  void (*delete)(Object* this);
-  void (*copy)(Object* this);
-};
-
 Object* Object_new()
 {
   Object* result = NULL;
--- src/util/Object.h
+++ src/util/Object.h
@@ -1,6 +1,17 @@
 /* Object.h */
 
+#ifndef _OBJECT_
+#define _OBJECT_
+
 typedef struct Object Object;
+
+struct Object
+{
+  void (*delete)(Object* this);
+  void (*copy)(Object* this);
+};
 
 Object* Object_new();
 void Object_delete(Object* this);
+
+#endif
