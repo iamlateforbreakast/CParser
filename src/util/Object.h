f510aebbac138d44eb7ff3a5fddaec9dee9dd5b4
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-01-08 17:45:20 +0000

    Add refCount to structure

--- src/util/Object.h
+++ src/util/Object.h
@@ -3,15 +3,18 @@
 #ifndef _OBJECT_
 #define _OBJECT_
 
+#include "Common.h"
+
 typedef struct Object Object;
 
 struct Object
 {
   void (*delete)(Object* this);
   void (*copy)(Object* this);
+  unsigned int refCount;
 };
 
-Object* Object_new();
-void Object_delete(Object* this);
+PUBLIC Object* Object_new();
+PUBLIC void Object_delete(Object* this);
 
 #endif
