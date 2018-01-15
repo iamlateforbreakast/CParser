36d6fbe0023931e799ef60c9380be2bf6ecfde38
iamlateforbreakfast <iamlateforbreakast@users.noreply.github.com>
2018-01-15 17:48:40 +0000

    Initial revision

--- test/funct_pointer/main.c
+++ test/funct_pointer/main.c
@@ -1,1 +1,12 @@
+int addInt(int n, int m)
+{
+  return m+n;
+}
 
+typedef int (*myFuncDef)(int, int);
+
+myFuncDef functionFactory(int n) 
+{
+  myFuncDef functionPtr = addInt;
+  return functionPtr;
+}
